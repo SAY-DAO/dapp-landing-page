eefd545bb016621bb9c0b3e1c44d3783be9a14eb
unknown <ehsan@say.company>
2020-10-20 23:59:04 +0330

    react is connected to smart contract

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from "react";
+import loadSmartContract from "../getWeb3";
 
-const App = () => {
-    return(
-        <div>App</div>
-    );
+
+
+
+class App extends Component {
+    state = { web3: null, accounts: null, contract: null};
+
+    componentDidMount = async () => {
+        const { web3, accounts, nakama } = await loadSmartContract();
+        this.setState({web3, accounts, contract:nakama})
+        console.log(nakama)
+
+    }
+
+    render() {
+        if (!this.state.web3) {
+            return <div>Loading Web3, accounts, and contract...</div>;
+        }
+        return (
+            <div className="App">
+                <div>
+                    <div>
+                        <h2>Mint a Nakama</h2>
+                        <form>
+                            <div>Child</div>
+                            <div>Need</div>
+                            <div>Need Cost</div>
+                            <input />
+                        </form>
+                        <button>Pay with ETH</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,4 +1,5 @@
 import Web3 from "web3";
+import Nakama from "./contracts/Nakama.json";
 
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
@@ -10,6 +11,8 @@ const getWeb3 = () =>
         try {
           // Request account access if needed
           await window.ethereum.enable();
+          // console.log(accounts)
+
           // Acccounts now exposed
           resolve(web3);
         } catch (error) {
@@ -35,4 +38,33 @@ const getWeb3 = () =>
     });
   });
 
-export default getWeb3;
+
+const loadSmartContract = async () => {
+  try {
+    // Get network provider and web3 instance.
+    const web3 = await getWeb3();
+
+    // Use web3 to get the user's accounts.
+    const accounts = await web3.eth.getAccounts();
+
+
+    // Get the contract instance.
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = Nakama.networks[networkId];
+    const nakama = new web3.eth.Contract(
+        Nakama.abi,
+        deployedNetwork && deployedNetwork.address,
+    );
+    return {web3, accounts, nakama}
+  } catch (error) {
+    // Catch any errors for any of the above operations.
+    alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+    );
+    console.error(error);
+  }
+
+};
+
+
+export default loadSmartContract;
