65aa2f4c422004f4d8936c3dc923e248b501d144
unknown <ehsan@say.company>
2020-10-20 22:09:55 +0330

    basic miniting and testing

--- contracts/Nakama.sol
+++ contracts/Nakama.sol
@@ -9,6 +9,29 @@ contract Nakama is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    mapping(string => bool) _doneNeeds;
+    address [] donors;
+
     constructor() public ERC721("Nakama", "NAK") {}
+
+    function awardItem(address _donor, string memory _tokenURI) public returns (uint256) {
+        require(!_doneNeeds[_tokenURI]);
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+//        Mints tokenId and transfers it to to.
+        _safeMint(_donor, newItemId);
+        donors.push(_donor);
+//        Sets _tokenURI as the tokenURI of tokenId.
+        _setTokenURI(newItemId, _tokenURI);
+//        One token per need
+        _doneNeeds[_tokenURI] = true;
+
+
+        return newItemId;
+    }
+
+    function getDonors()  public  view returns( address [] memory ) {
+    return donors;
+}
 
 }
--- test/Nakama.test.js
+++ test/Nakama.test.js
@@ -5,33 +5,82 @@ require('chai')
     .should()
 
 contract('Nakama', (accounts) => {
-    let contract
+        let contract
 
-    before(async () => {
-        instance = await Nakama.deployed();
-    });
+        before(async () => {
+             nakama = await Nakama.deployed();
 
-    describe('deployment', async () => {
-
-        it('deploys successfully', async () => {
-            const address = instance.address;
-            console.log(address);
-            assert.notEqual(address, 0x0);
-            assert.notEqual(address, '');
-            assert.notEqual(address, null);
-            assert.notEqual(address, undefined);
+             tokenURIs = [
+                 'https://sayapp.company/child/1/need/45',
+                 'https://sayapp.company/child/56/need/6',
+                 'https://sayapp.company/child/12/need/5',
+                 'https://sayapp.company/child/13/need/15',
+                 'https://sayapp.company/child/2/need/23'
+                 ]
         });
 
-        it('has a name', async () => {
-            const name = await instance.name();
-            assert.equal(name, 'Nakama');
-        });
+        describe('deployment', async () => {
 
-        it('has a symbol', async () => {
-            const symbol = await instance.symbol();
-            assert.equal(symbol, 'NAK');
-        });
-    })
+            it('deploys successfully', async () => {
+                const address = nakama.address;
+                assert.notEqual(address, 0x0);
+                assert.notEqual(address, '');
+                assert.notEqual(address, null);
+                assert.notEqual(address, undefined);
+            });
 
-}
+            it('has a name', async () => {
+                const name = await nakama.name();
+                assert.equal(name, 'Nakama');
+            });
+
+            it('has a symbol', async () => {
+                const symbol = await nakama.symbol();
+                assert.equal(symbol, 'NAK');
+            });
+        })
+
+        describe('minting', async () => {
+            it('creates a new token for accounts[0]', async () => {
+
+
+                const token = await nakama.awardItem(accounts[0], tokenURIs[0]);
+                const totalSupply = await nakama.totalSupply()
+                assert.equal(totalSupply, 1);
+
+                const result = token.logs[0].args;
+                assert.equal(result.tokenId.toNumber(), 1, 'id is correct');
+                assert.equal(result.from, '0x0000000000000000000000000000000000000000', 'from is correct');
+                assert.equal(result.to, accounts[0], 'to is correct');
+
+                // Failure - Same need should not be rewarded
+                await nakama.awardItem(accounts[0], tokenURIs[0]).should.be.rejected;
+            })
+        })
+
+        describe('indexing', async () => {
+            it('lists done needs', async() => {
+            //    Mint 3 more tokens
+                await nakama.awardItem(accounts[0], tokenURIs[1]);
+                await nakama.awardItem(accounts[0], tokenURIs[2]);
+                await nakama.awardItem(accounts[1], tokenURIs[3]);
+
+                const totalSupply = await nakama.totalSupply();
+                let tokenURI;
+                let result = [];
+
+                for (var i=1; i<=totalSupply; i++) {
+                    tokenURI = await nakama.tokenURI(i);
+                    result.push(tokenURI)
+                }
+
+                let expected = [tokenURIs[0], tokenURIs[1], tokenURIs[2], tokenURIs[3]];
+                assert.equal(result.join(','), expected.join(','));
+
+
+
+
+            })
+        })
+    }
 )
